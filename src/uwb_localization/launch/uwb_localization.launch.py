6661fb856e109ef097f5cccab2e152a3746ccfee
giorgiomanca97 <giorgio.manca.97@gmail.com>
2024-07-25 11:35:50 +0200

    feat: publisher, verbose mode, config, launch files

--- src/uwb_localization/launch/uwb_localization.launch.py
+++ src/uwb_localization/launch/uwb_localization.launch.py
@@ -1,23 +1,71 @@
-# from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch_ros.actions import Node
+"""
+UWB Localization app launch file.
+
+Giorgio Manca <giorgio.manca97@gmail.com>
+
+July 24, 2024
+"""
+
+# Copyright 2024 dotX Automation s.r.l.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
+from ament_index_python.packages import get_package_share_directory
 
-import sys
-params_holder_path = '/home/neo/workspace/src/params_holder'
-sys.path.insert(1, params_holder_path)
-import update_yaml
-update_yaml.update_params()
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
 
-config = os.path.join(params_holder_path, 'config/params_holder.yaml')
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='uwb_localization',
-            executable='uwb_localization',
-            name='uwb_localization',
-            output='screen',
-            parameters=[config]
-        ),
-    ])
+    ld = LaunchDescription()
+
+    # Build config file path
+    config = os.path.join(
+        get_package_share_directory('uwb_localization'),
+        'config',
+        'uwb_localization.yaml')
+
+    # Declare launch arguments
+    nm = LaunchConfiguration('name')
+    ns = LaunchConfiguration('namespace')
+    cf = LaunchConfiguration('cf')
+    nm_launch_arg = DeclareLaunchArgument(
+        'name',
+        default_value='uwb_localization')
+    ns_launch_arg = DeclareLaunchArgument(
+        'namespace',
+        default_value='')
+    cf_launch_arg = DeclareLaunchArgument(
+        'cf',
+        default_value=config)
+    ld.add_action(nm_launch_arg)
+    ld.add_action(ns_launch_arg)
+    ld.add_action(cf_launch_arg)
+
+    # Create node launch description
+    node = Node(
+        package='uwb_localization',
+        executable='uwb_localization_app',
+        name=nm,
+        namespace=ns,
+        emulate_tty=True,
+        output='both',
+        log_cmd=True,
+        parameters=[cf])
+
+    ld.add_action(node)
+
+    return ld
--- src/uwb_localization/src/uwb_localization/ul_subscriptions.cpp
+++ src/uwb_localization/src/uwb_localization/ul_subscriptions.cpp
@@ -7,19 +7,19 @@
  */
 
 /**
- * This is free software.
- * You can redistribute it and/or modify this file under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 3 of the License, or (at your option) any later
- * version.
+ * Copyright 2024 dotX Automation s.r.l.
  *
- * This file is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * You should have received a copy of the GNU General Public License along with
- * this file; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <uwb_localization/uwb_localization.hpp>
@@ -34,6 +34,8 @@ namespace uwb_localization
  */
 void UWBLocalizationNode::uwb_clbk(const UWBTag::SharedPtr msg)
 {
+  rclcpp::Time stamp = this->get_clock()->now();
+
   TransformStamped tf_global_base;
   TransformStamped tf_base_uwb;
   try {
@@ -97,6 +99,13 @@ void UWBLocalizationNode::uwb_clbk(const UWBTag::SharedPtr msg)
   pos_[0] = result.position[0] - traslation.x();
   pos_[1] = result.position[1] - traslation.y();
   pos_[2] = result.position[2] - traslation.z();
+
+  publish_pose(stamp);
+
+  if(verbose_) {
+    RCLCPP_INFO(this->get_logger(), "Estimation: %0.3f %0.3f %0.3f", pos_[0], pos_[1], pos_[2]);
+    std::cout << result.summary.BriefReport() << std::endl;
+  }
 }
 
 
